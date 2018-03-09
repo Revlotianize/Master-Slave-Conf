2b6fd242a68eda56e74c7fbd083be241b69f37bf
Kartik Madnani <kartik.madnani123@gmail.com>
2018-03-09 11:16:17 +0530

    mvc formatted

--- controllers/DetailsController.php
+++ controllers/DetailsController.php
@@ -5,9 +5,7 @@ namespace app\controllers;
 use Yii;
 use yii\web\Controller;
 use yii\helpers\ArrayHelper;
-// use app\controllers\Details;
 use app\models\Details;
-// use app\controllers\PDO;
 use yii\db\Query;
 
 class DetailsController extends \yii\web\Controller
@@ -17,53 +15,22 @@ class DetailsController extends \yii\web\Controller
     public function actionIndex()
     {
     	$model = new Details();
-
-    	$data = ArrayHelper::map(Details::find()->select(['tablename'])->asArray()->where(['status'=>'1'])->all(), 'tablename', 'tablename');
-    	
-    	$detail =ArrayHelper::map(Details::find()->select(['tablename'])->asArray()->where(['location'=>"slave"])->all(),'tablename','tablename');
+	 	$data = $model->getTable();
+    	$detail = $model->getSlave();
 		
-    	if($model->load(\Yii::$app->request->post()) && $model->validate());
-    	{
-        	
         	return $this->render('index',[
         	'model'=>$model,
         	'data'=>$data,
         	'detail'=>$detail,
         	]);
 
-    	}
 	}
 
 
 	public function actionExecute()
-	{
-		
-		$postdata = $_POST;
-		if($postdata['Details']['slave'] == null )
-		{
-			$connection = \Yii::$app->db2;
-			$query = $postdata['Details']['query'];	
-			$res = $connection->createCommand($query);
-			$users = $res->queryAll();
-		;
-		}
-
-		elseif ($postdata['Details']['slave'] == 'jd') {
-			$connection = \Yii::$app->db3;
-			$query = $postdata['Details']['query'];
-			$res = $connection->createCommand(mysql_real_escape_string(trim($query)));
-			$users = $res->queryAll();
-			return "success!";
-	
-		}
-		elseif ($postdata['Details']['slave'] == 'madras') {
-			$connection = \Yii::$app->db4;
-			$query = $postdata['Details']['query'];			
-			$res = $connection->createCommand(mysql_real_escape_string(trim($query)));
-			$users = $res->queryAll();
-			
-		}
-		
+	{				
+    	$model = new Details();
+		$model->check();	
 	}		
 }
 
--- models/Details.php
+++ models/Details.php
@@ -3,6 +3,7 @@
 namespace app\models;
 
 use Yii;
+use yii\helpers\ArrayHelper;
 use app\models\Details;
 /**
  * This is the model class for table "details".
@@ -27,9 +28,40 @@ class Details extends \yii\db\ActiveRecord
         return 'details';
     }
 
-    public function connection()
+    public function getTable()
     {
-        
+        $data = ArrayHelper::map(Details::find()->select(['tablename'])->asArray()->where(['status'=>'1'])->all(), 'tablename', 'tablename');               
+        return $data;        
+    }
+    public function  getSlave()
+    {
+         $detail =ArrayHelper::map(Details::find()->select(['tablename'])->asArray()->where(['location'=>"slave"])->all(),'tablename','tablename');
+         return $detail;
+    }
+
+    public function check()
+    {
+        $postdata = $_POST;
+        if($postdata['Details']['slave'] == null ) {
+            $connection = \Yii::$app->db2;
+            $query = $postdata['Details']['query']; 
+            $res = $connection->createCommand($query);
+            $users = $res->queryAll();
+        }
+
+        elseif ($postdata['Details']['slave'] == 'jd') {
+            $connection = \Yii::$app->db3;
+            $query = $postdata['Details']['query'];
+            $res = $connection->createCommand($query);
+            $users = $res->queryAll();
+    
+        }
+        elseif ($postdata['Details']['slave'] == 'madras') {
+            $connection = \Yii::$app->db4;
+            $query = $postdata['Details']['query'];         
+            $res = $connection->createCommand($query);
+            $users = $res->queryAll();      
+        }
     }
     /**
      * @inheritdoc
